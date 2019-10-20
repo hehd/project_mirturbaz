4753bcfff7349e0009ce0c0125391bdc5ffe8502
hehd <nmirth@mail.ru>
2019-10-20 16:32:55 +0400

    Добавленны методы CRUD в контроллер стран и созданы вьюхи

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def render_404
+    render file: "public/404.html", status: 404
+  end
+
 end
--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,50 @@
 class CountriesController < ApplicationController
+
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @country = Country
+    @country = @country.order("id")
+  end
+
+  def show
+  end
+
+  def create
+    @country = Country.create(params[:country].permit!)
+    if @country.errors.empty?
+      redirect_to country_path(@country)
+    else
+      render "new"
+    end
+  end
+
+  def new
+    @country = Country.new
+  end
+
+  def edit
+  end
+
+  def update
+    @country.update_attributes(params[:country].permit!)
+    if @country.errors.empty?
+      redirect_to country_path(@country)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @country.destroy
+    redirect_to action: "index"
+  end
+
+  private
+
+  def find_item
+    @country = Country.where(id: params[:id]).first
+    render_404 unless @country
+  end
+
 end
