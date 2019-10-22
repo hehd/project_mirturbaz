eebe84ea80fafc4d6d2a992e0a5d8036354b2137
hehd <nmirth@mail.ru>
2019-10-23 01:46:12 +0400

    добавлен контроллер для турбаз

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :countries
   resources :regions
   resources :cities
+  resources :camps
   get 'main/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
