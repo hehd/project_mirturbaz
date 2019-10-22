f455fecc715c213dcba7b02480287a7e4df49f58
hehd <nmirth@mail.ru>
2019-10-22 11:53:33 +0400

    добавлен контроллер для регионов

---
+++ app/controllers/regions_controller.rb
@@ -0,0 +1,50 @@
+class RegionsController < ApplicationController
+
+  before_action :find_region, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @region = Region
+    @region = @region.order("id")
+  end
+
+  def show
+  end
+
+  def create
+    @region = Region.create(params[:region].permit!)
+    if @region.errors.empty?
+      redirect_to region_path(@region)
+    else
+      render "new"
+    end
+  end
+
+  def new
+    @region = Region.new
+  end
+
+  def edit
+  end
+
+  def update
+    @region.update_attributes(params[:region].permit!)
+    if @region.errors.empty?
+      redirect_to region_path(@region)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @region.destroy
+    redirect_to action: "index"
+  end
+
+  private
+
+  def find_region
+    @region = Region.where(id: params[:id]).first
+    render_404 unless @region
+  end
+
+end
