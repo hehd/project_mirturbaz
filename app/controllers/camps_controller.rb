1e905f7598bafd176345124b54d89b92f41ee504
hehd <nmirth@mail.ru>
2019-10-23 02:35:06 +0400

    Добавлены методы CRUD для турбаз

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -1,2 +1,50 @@
 class CampsController < ApplicationController
+
+  before_action :find_camp, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @camp = Camp
+    @camp = @camp.order("id")
+  end
+
+  def show
+  end
+
+  def create
+    @camp = Camp.create(params[:camp].permit!)
+    if @camp.errors.empty?
+      redirect_to camp_path(@camp)
+    else
+      render "new"
+    end
+  end
+
+  def new
+    @camp = Camp.new
+  end
+
+  def edit
+  end
+
+  def update
+    @camp.update_attributes(params[:camp].permit!)
+    if @camp.errors.empty?
+      redirect_to camp_path(@camp)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @camp.destroy
+    redirect_to action: "index"
+  end
+
+  private
+
+  def find_camp
+    @camp = Camp.where(id: params[:id]).first
+    render_404 unless @camp
+  end
+
 end
