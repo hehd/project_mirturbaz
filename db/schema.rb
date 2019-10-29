c075d6dd32b29ec853f3f9b1bb975b9ed5cebc93
hehd <nmirth@mail.ru>
2019-10-29 18:42:12 +0400

    измененная схема

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_210401) do
+ActiveRecord::Schema.define(version: 2019_10_29_143051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_10_19_210401) do
     t.integer "city_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["name"], name: "index_camps_on_name"
     t.index ["price"], name: "index_camps_on_price"
   end
