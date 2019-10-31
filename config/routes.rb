5998cd0d0554d0d743b8ed57e69328bf3fc3cc01
hehd <nmirth@mail.ru>
2019-10-31 15:43:15 +0400

    незначительные изменения

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'main#index'
   resources :countries
   resources :regions
   resources :cities
   resources :camps
-  get 'main/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
